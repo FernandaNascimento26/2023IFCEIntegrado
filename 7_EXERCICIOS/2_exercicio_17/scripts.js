8e1d4be728cf4c1ee2c67d8a6aeffc015124229f
FernandaNascimento26 <nandinha.cefet@hotmail.com>
2023-05-15 11:28:41 -0300

    add functions examples

---
+++ 7_EXERCICIOS/2_exercicio_17/scripts.js
@@ -0,0 +1,10 @@
+
+let idade = parseInt(prompt("digite sua idade"))
+function imprimeIdade(idade) {
+  console.log(`Você tem ${idade} anos`);
+}
+
+imprimeIdade(12);
+imprimeIdade(28);
+imprimeIdade(45);
+imprimeIdade(idade);
